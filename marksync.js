1163132439a6d3045bf880564000ecc958486b53
Alex Coad <javaboy132@gmail.com>
2017-11-28 00:48:55 +0000

    work on bookmarks permissions

--- marksync.js
+++ marksync.js
@@ -1,2 +1,13 @@
-var bookmarx=bookmarks.getTree();
-console.log("hello");
+
+function SaveTree(bookmarkItems){
+
+}
+function onRejected(error){
+	console.log('An error: $(error)');
+}
+
+console.log("starting up");
+var bookmarx=browser.bookmarks.getTree();
+console.log("bookmarks loading");
+bookmarx.then(SaveTree,onRejected);
+console.log("all done");
